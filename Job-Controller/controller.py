a6f945c1de0fcfc5d771749fcf7753f7f6d7231d
Srinibas Misra <srinibas.misra97@gmail.com>
2021-02-09 13:52:21 +0530

    fixed resource already exists issue

--- Job-Controller/controller.py
+++ Job-Controller/controller.py
@@ -1,10 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from kubernetes import client
 import base64
 from tempfile import NamedTemporaryFile
 import os
 import yaml
+import string
+import random
 
 app = Flask(__name__)
 
@@ -20,7 +22,6 @@ configuration.host = HOST_URL
 configuration.verify_ssl = True
 configuration.debug = False
 configuration.api_key = { "authorization": "Bearer {}".format(base64.b64decode(TOKEN).decode("utf-8")) }
-print(configuration.api_key)
 client.Configuration.set_default(configuration)
 
 v1 = client.BatchV1Api()
@@ -33,14 +34,18 @@ def root():
 def trigger_job1():
     with open("job1.yaml") as file:
         body = yaml.safe_load(file)
+        body['metadata']['name'] = "job1-{}".format(''.join(random.choices(string.ascii_lowercase, k = 5)))
         
     v1.create_namespaced_job(namespace="default", body=body, pretty=True)
     return "Job triggered!"
 
 @app.route("/trigger/job2")
 def trigger_job2():
+    process_folder = request.args.get("folder")
     with open("job2.yaml") as file:
         body = yaml.safe_load(file)
+        body['metadata']['name'] = "job2-{}".format(''.join(random.choices(string.ascii_lowercase, k = 5)))
+        body['spec']['template']['spec']['containers'][0]['env'][0]['value'] = process_folder
         
     v1.create_namespaced_job(namespace="default", body=body, pretty=True)
     return "Job triggered!"
